3659545d8b2f32e7d4d120acf9b2e2068a5f0b4b
Kronifer <44979306+Kronifer@users.noreply.github.com>
2025-11-23 18:13:51 -0600

    fixed download_tiles

--- patcher/packages/mapPatcher/download_tiles.js
+++ patcher/packages/mapPatcher/download_tiles.js
@@ -4,8 +4,5 @@ import config from './config.js';
 console.log("Downloading map tiles");
 for(var place of config.places) {
     console.log(`Fetching tiles for ${place.name} (${place.code})`);
-    if(config.platform === 'windows')
-        execSync(`${import.meta.dirname}\\..\\map_tiles\\pmtiles.exe extract ${config['protomaps-bucket']} --maxzoom=${config['tile-zoom-level']} --bbox="${place.bbox.join(',')}" ./map_tiles/${place.code}.pmtiles`);
-    else if(config.platform === 'linux' || config.platform === 'macos') 
-        execSync(`${import.meta.dirname}/../map_tiles/pmtiles extract ${config['protomaps-bucket']} --maxzoom=${config['tile-zoom-level']} --bbox="${place.bbox.join(',')}" ./map_tiles/${place.code}.pmtiles`);   
+    execSync(`${import.meta.dirname}/./map_tiles/pmtiles extract ${config['protomaps-bucket']} --maxzoom=${config['tile-zoom-level']} --bbox="${place.bbox.join(',')}" ${import.meta.dirname}/./map_tiles/${place.code}.pmtiles`);   
 }
