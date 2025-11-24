ff7f1cbe9af45bd28741eecc0bee3d33628a6e62
rslurry <112521174+rslurry@users.noreply.github.com>
2025-11-24 03:25:04 -0500

    Updated config_linux.js to use linux in the config

--- config_linux.js
+++ config_linux.js
@@ -1,6 +1,6 @@
 const config = {
   "subwaybuilderLocation": "Path/To/Game.AppImage", // appimage location image on linux or install directory on windows (something like C:\\Users\\[username]\\AppData\\Local\\Programs\\Subway\ Builder)
-  "platform": "windows", // either 'linux' or 'windows'
+  "platform": "linux", // either 'linux' or 'windows'
   "packagesToRun": ["mapPatcher"]
 };
 
