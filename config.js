0699c210dde622ec5bf5ab65c155346dff677339
mhmoeller <mhegelund_moeller@hotmail.com>
2025-11-28 22:43:44 +0100

    Forgot to add download_tools.js

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 const config = {
-  "subwaybuilderLocation": "C:\\Users\\runke\\AppData\\Local\\Programs\\Subway\ Builder\\", // appimage location image on linux or install directory on windows (something like C:\\Users\\[username]\\AppData\\Local\\Programs\\Subway\ Builder)
-  "platform": "windows", // either 'linux' or 'windows'
-  "packagesToRun": ["mapPatcher"]
+  "subwaybuilderLocation": "C:\\Users\\mhege\\AppData\\Local\\Programs\\Subway Builder\\", 
+  "platform": "windows", 
+  "packagesToRun": [
+  "mapPatcher"
+]
 };
 
 export default config;
