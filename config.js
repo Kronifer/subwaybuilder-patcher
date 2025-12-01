e086b56c78c624d2adfa004359e86372a4a5716c
Joseph Wong <josephw0823@gmail.com>
2025-11-30 17:56:46 -0800

    revert to original

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 // See config_macos, config_linux, config_linux for examples
 const config = {
-  "subwaybuilderLocation": "/Applications/Subway Builder.app", // app location (or something like /Users/[username]/Applications/Subway Builder.app')
-  "platform": "macos", // 'macos', 'linux' or 'windows'
+  "subwaybuilderLocation": "C:\\Users\\runke\\AppData\\Local\\Programs\\Subway Builder\\", 
+  "platform": "windows", // 'macos', 'linux' or 'windows'
   "packagesToRun": [
-    "mapPatcher",
-    "addTrains"
+    "mapPatcher"
   ]
 };
 
