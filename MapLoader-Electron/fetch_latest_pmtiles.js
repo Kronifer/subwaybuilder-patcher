f141582f94e476ee4e3613c7ce44d58f9bd44278
Kronifer <44979306+Kronifer@users.noreply.github.com>
2026-02-20 22:28:39 -0600

    make pmtiles executable in CI and hope it carries over

--- MapLoader-Electron/fetch_latest_pmtiles.js
+++ MapLoader-Electron/fetch_latest_pmtiles.js
@@ -4,28 +4,56 @@ const request = require("request");
 const { Readable } = require("node:stream");
 
 async function fetchLatestPMTiles() {
-    let ghApiRequest = await fetch("https://api.github.com/repos/protomaps/go-pmtiles/releases");
-    let body = await ghApiRequest.json();
-    console.log(body);
-    let tag = body[0].tag_name;
-    let url = `https://github.com/protomaps/go-pmtiles/releases/download/${tag}/go-pmtiles`
-    switch (process.platform) {
-        case "win32":
-            url += `_${tag.replaceAll("v", "")}_Windows_${process.arch === "x64" ? "x86_64" : "arm64"}.zip`;
-            console.log("Fetching PMTiles from " + url);
-            unzipper.Open.url(request, url).then(d => d.extract({ path: "./", filter: file => file.path.endsWith("pmtiles.exe") }));
-            break
-        case "darwin":
-            url += `-${tag.replaceAll("v", "")}_Darwin_${process.arch === "x64" ? "x86_64" : "arm64"}.zip`;
-            unzipper.Open.url(request, url).then(d => d.extract({ path: "./", filter: file => file.path.endsWith("pmtiles") }));
-            break
-        default:
-            url += `_${tag.replaceAll("v", "")}_Linux_${process.arch === "x64" ? "x86_64" : "arm64"}.tar.gz`;
-            fetch(url).then(res => {
-                Readable.fromWeb(res.body).pipe(tar.extract({ filter: path => path.endsWith("pmtiles"), cwd: "./" }));
-            });
-            break
-    }
+  let ghApiRequest = await fetch(
+    "https://api.github.com/repos/protomaps/go-pmtiles/releases",
+  );
+  let body = await ghApiRequest.json();
+  console.log(body);
+  let tag = body[0].tag_name;
+  let url = `https://github.com/protomaps/go-pmtiles/releases/download/${tag}/go-pmtiles`;
+  switch (process.platform) {
+    case "win32":
+      url += `_${tag.replaceAll("v", "")}_Windows_${process.arch === "x64" ? "x86_64" : "arm64"}.zip`;
+      console.log("Fetching PMTiles from " + url);
+      unzipper.Open.url(request, url).then((d) =>
+        d.extract({
+          path: "./",
+          filter: (file) => file.path.endsWith("pmtiles.exe"),
+        }),
+      );
+      break;
+    case "darwin":
+      url += `-${tag.replaceAll("v", "")}_Darwin_${process.arch === "x64" ? "x86_64" : "arm64"}.zip`;
+      let zip = unzipper.Open.url(request, url);
+      zip.then((d) =>
+        d
+          .extract({
+            path: "./",
+            filter: (file) => file.path.endsWith("pmtiles"),
+          })
+          .then(() => {
+            // Make the file executable
+            exec("chmod +x pmtiles");
+          }),
+      );
+      break;
+    default:
+      url += `_${tag.replaceAll("v", "")}_Linux_${process.arch === "x64" ? "x86_64" : "arm64"}.tar.gz`;
+      fetch(url).then((res) => {
+        Readable.fromWeb(res.body)
+          .pipe(
+            tar.extract({
+              filter: (path) => path.endsWith("pmtiles"),
+              cwd: "./",
+            }),
+          )
+          .on("finish", () => {
+            // Make the file executable
+            exec("chmod +x pmtiles");
+          });
+      });
+      break;
+  }
 }
 
 fetchLatestPMTiles();
